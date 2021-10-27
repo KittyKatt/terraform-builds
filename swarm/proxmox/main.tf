e1c2c7a54cb27e38c687d6c7be4a69e2ec5a857b
KittyKatt <kittykatt@kittykatt.co>
2021-10-27 15:43:19 -0500

    Adding Proxmox provider

--- swarm/proxmox/main.tf
+++ swarm/proxmox/main.tf
@@ -1,3 +1,5 @@
+
+
 module "cluster" {
   source            = "./modules/cluster"
 
@@ -6,17 +8,3 @@ module "cluster" {
   smbusername       = var.smbusername
   smbpassword       = var.smbpassword
 }
-
-# module "cloud-config" {
-#     source = "./modules/config"
-
-#     name           = "moradin"
-#     ip_address     = "10.1.10.41"
-#     manager        = true
-#     network_config = var.network
-#     mount_config   = local.mounts
-#     ssh_ids        = var.ssh_id
-#     ssh_keys       = var.ssh_key
-#     smbuser        = var.smbusername
-#     smbpass        = var.smbpassword
-# }
---
+++ swarm/proxmox/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "2.8.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_tls_insecure = true
+  pm_api_url = var.pm_url
+  pm_user = var.pm_user
+  pm_password = var.pm_password
+}
